046f09241b0891b5fcd4543e77946bd6661e30fd
Maxim-Lichtenberger <mlichtenberg.ieu2023@student.ie.edu>
2025-01-26 11:52:53 +0100

    v1 Created first draft

---
+++ main.py
@@ -0,0 +1,29 @@
+# Instructions
+# - Given the “gross” salary of a person, calculate the “net”.
+# Here are the constraints:
+# 1.If the gross is less than 1000, then the income tax is 10%
+# 2.If the gross is less than 2000, then the income tax is 12%
+# 3.If the gross is less than 4000, then the income tax is 14%
+# 4.If the gross is more than 4000, then the income tax is 18%
+# 5.If the gross is less than 2000, every child gets you a tax cut of 1%
+# 6.If the gross is more than 2000, every child gets you a tax cut of 0.5%
+# Read the “gross” and the number of children
+# Print the “net”
+# Use try/except when reading the inputs '
+
+print("Welcome to Lichtenberger Tax Advisory. Please answer the following questions, so we can create your tax report:")
+
+try:
+    gross_salary = input("What is your gross salary?")
+    no_kids = int(input("How many children do you have?"))
+    if gross_salary < 1000:
+        tax_rate = 0.1 - (0.01 * no_kids)
+    elif gross_salary < 2000 & no_kids = 0:
+        tax_rate = 0.12 - (0.01 * no_kids)
+    elif gross_salary < 4000 & no_kids = 0:
+        tax_rate = 0.14 - (0.005 * no_kids)
+    elif gross_salary > 4000 & no_kids = 0:
+        tax_rate = 0.18 - (0.005 * no_kids)
+        print(f"Your tax rate is {tax_rate}. You need to pay the IRS {(tax_rate)*gross_salary}")
+except ValueError:
+    print("Please enter a valid number.")
